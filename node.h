82565c0d170ddc6552bc6ac1d56e77ae522b73c3
Hyungjoo Park <copyrose97@gmail.com>
2020-06-28 15:10:45 +0900

    change cpp to hpp to link template code

--- node.h
+++ node.h
@@ -1,6 +1,6 @@
+#include <utility>
 #ifndef NODE_H
 #define NODE_H
-#include <utility>
 template <typename _key, typename _value>
 struct _obj{
 	_key key;
@@ -18,16 +18,15 @@ struct _obj{
 };
 
 template <typename _key, typename _value>
-class _node{
+struct _node{
     int num;
 	_obj<_key, _value>* val[3];
 	_node* child[4]; //if child exist -> child[0]!=NULL
 	_node* parent; 
 public:
 	_node(void){
-        temp->parent = NULL;
-        temp->num = 0;
-        temp->child = {NULL};
+        parent = nullptr;
+        num = 0;
     }
 };
 #endif
